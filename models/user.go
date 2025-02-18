014eb7927b19c88046352c5fd7c84f61ee2734ef
irsanrasyidin <nasrirasyidin@gmail.com>
2025-02-18 10:55:36 +0700

    separate entity into user and user_crendtial

--- models/user.go
+++ models/user.go
@@ -1,11 +1,5 @@
 package models
 
-type UserCredential struct {
-	ID       string `json:"id" gorm:"column:id"`
-	Username string `json:"username" gorm:"column:username"`
-	Password string `json:"password" gorm:"column:password"`
-}
-
 type UserModel struct {
 	ID       string `json:"id" gorm:"column:id"`
 	Username string `json:"username" gorm:"column:username"`
